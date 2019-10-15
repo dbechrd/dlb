323c2a1d9c58b83fe819cdf66e22749f280c8210
Dan Bechard <dbechrd@gmail.com>
2019-10-15 03:30:30 -0700

    Sparse list assume unique, mostly contiguous entity ids

--- dlb_pool.h
+++ dlb_pool.h
@@ -9,19 +9,14 @@
 #include "dlb_types.h"
 #include "dlb_memory.h"
 
-typedef struct dlb_id {
-    u32 index;
-    u32 generation;
-} dlb_id;
-
 // Fixed-size pool with intrusive free list
 typedef struct dlb_pool {
     u32 capacity;       // number of slots allocated
     u32 size;           // number of slots occupied
     u32 elem_size;      // size of each element in dense set
-    u32 free_head;      // first free sparse slot, == capacity when pool is full
-    dlb_id *sparse_set; // indices into dense (do not reorder, do not shrink)
-    void *dense_set;    // tightly packed data for fast iteration
+    u32 *sparse_set;    // uid -> dense index (do not reorder, do not shrink)
+    u32 *dense_set;     // dense set of uids
+    void *dense_data;   // tightly packed data for fast iteration
 } dlb_pool;
 
 void dlb_pool_reserve(dlb_pool *pool, u32 capacity);
@@ -29,92 +24,88 @@ void dlb_pool_reserve(dlb_pool *pool, u32 capacity);
 static inline void dlb_pool_init(dlb_pool *pool, u32 elem_size, u32 capacity)
 {
     // Elements must be at least large enough to hold a freelist
-    DLB_ASSERT(elem_size >= sizeof(u32));
     pool->elem_size = elem_size;
     dlb_pool_reserve(pool, capacity);
 }
 
+// WARNING: Direct access to dense array. Should only be used for fast iteration
 static inline void *dlb_pool_at(dlb_pool *pool, u32 dense_index)
 {
-    DLB_ASSERT(pool->size);
-    DLB_ASSERT(pool->dense_set);
+    DLB_ASSERT(pool->dense_data);
+    DLB_ASSERT(dense_index < pool->size);
 
-    void *ptr = (u8 *)pool->dense_set + pool->elem_size * dense_index;
+    void *ptr = (u8 *)pool->dense_data + pool->elem_size * dense_index;
     return ptr;
 }
 
-static inline void *dlb_pool_retrieve(dlb_pool *pool, dlb_id id)
+// Retrieve by id, safest access pattern (verfies dense_set match)
+static inline void *dlb_pool_by_id(dlb_pool *pool, u32 id)
 {
-    DLB_ASSERT(id.index < pool->capacity);
-    DLB_ASSERT(pool->size);
-    DLB_ASSERT(pool->dense_set);
+    DLB_ASSERT(pool->sparse_set);
+    DLB_ASSERT(id < pool->capacity);
+    u32 dense_index = pool->sparse_set[id];
 
-    dlb_id dense_id = pool->sparse_set[id.index];
-    DLB_ASSERT(dense_id.generation == id.generation);
-    void *ptr = dlb_pool_at(pool, dense_id.index);
+    DLB_ASSERT(pool->dense_set);
+    DLB_ASSERT(dense_index < pool->size);
+    void *ptr = 0;
+    if (pool->dense_set[dense_index] == id) {
+        ptr = dlb_pool_at(pool, dense_index);
+    }
     return ptr;
 }
 
-static inline dlb_id dlb_pool_alloc(dlb_pool *pool, void **ptr)
+static inline void *dlb_pool_alloc(dlb_pool *pool, u32 id)
 {
-    if (pool->free_head == pool->capacity) {
+    if (pool->size == pool->capacity) {
         dlb_pool_reserve(pool, pool->capacity << 1);
     }
-    DLB_ASSERT(pool->free_head < pool->capacity);
     DLB_ASSERT(pool->size < pool->capacity);
 
-    dlb_id id = { 0 };
-    id.index = pool->free_head;
-    id.generation = pool->sparse_set[pool->free_head].generation;
-    pool->free_head = pool->sparse_set[pool->free_head].index;
-    pool->sparse_set[id.index].index = pool->size;
-
-    if (ptr) *ptr = dlb_pool_at(pool, pool->size);
+    void *ptr = dlb_pool_at(pool, pool->size);
+    pool->sparse_set[id] = pool->size;
+    pool->dense_set[pool->size] = id;
     pool->size++;
-    return id;
+    return ptr;
 }
 
-static inline void dlb_pool_delete(dlb_pool *pool, dlb_id id)
+static inline bool dlb_pool_delete(dlb_pool *pool, u32 id)
 {
-    DLB_ASSERT(id.index < pool->capacity);
-    DLB_ASSERT(pool->size);
+    DLB_ASSERT(pool->sparse_set);
+    DLB_ASSERT(id < pool->capacity);
+    u32 dense_index = pool->sparse_set[id];
+
     DLB_ASSERT(pool->dense_set);
-
-    dlb_id *dense_id = &pool->sparse_set[id.index];
-    DLB_ASSERT(dense_id->generation == id.generation);
-
-    u32 last_idx = pool->size - 1;
-    void *ptr = dlb_pool_at(pool, dense_id->index);
-    void *last = dlb_pool_at(pool, last_idx);
-
-    if (last != ptr) {
-        dlb_memcpy(ptr, last, pool->elem_size);
-
-        // TODO(perf): Faster way to update sparse set when filling holes? The
-        // only faster way I can think of is to add u32 overhead to dlb_id and
-        // store "prev", or store relative offsets. The former seems like a
-        // waste of space and the latter seems overly complicated.
-        for (u32 i = 0; i < pool->capacity; ++i) {
-            if (pool->sparse_set[i].index == last_idx) {
-                pool->sparse_set[i].index = dense_id->index;
-                break;
-            }
-        }
-    }
-    dlb_memset(last, 0, pool->elem_size);
-
-    // TODO: If generation runs out we can move deleted to end of free list
-    // instead of beginning to make reuse take much longer.
-    dense_id->index = pool->free_head;
-    dense_id->generation++;
-    pool->free_head = id.index;
+    DLB_ASSERT(dense_index < pool->size);
     pool->size--;
+
+    bool found = false;
+    if (pool->dense_set[dense_index] == id) {
+        found = true;
+        void *ptr = dlb_pool_at(pool, dense_index);
+        void *last_ptr = dlb_pool_at(pool, pool->size);
+
+        if (ptr != last_ptr) {
+            // Compact (move last element to newly empty slot)
+            u32 last_id = pool->dense_set[pool->size];
+            pool->sparse_set[last_id] = dense_index;
+            pool->dense_set[dense_index] = last_id;
+            dlb_memcpy(ptr, last_ptr, pool->elem_size);
+        }
+
+        // TODO(perf): None of this is actually necessary, just doing for debug
+        // Clear deleted slot (invalidate sets)
+        pool->sparse_set[id] = 0xC0FFEE42;         // anything >= pool->size
+        pool->dense_set[pool->size] = 0xC0FFEE42;  // anything >= pool->size
+        dlb_memset(last_ptr, 0, pool->elem_size);
+    }
+    return found;
 }
 
 static inline void dlb_pool_free(dlb_pool *pool)
 {
-    dlb_free(pool->dense_set);
     dlb_free(pool->sparse_set);
+    dlb_free(pool->dense_set);
+    dlb_free(pool->dense_data);
 }
 
 #endif
@@ -135,12 +126,14 @@ void dlb_pool_reserve(dlb_pool *pool, u32 capacity)
     u32 old_capacity = pool->capacity;
     pool->capacity = capacity;
 
-    if (pool->dense_set) {
-        pool->dense_set = dlb_realloc(pool->dense_set, pool->capacity * pool->elem_size);
+    if (pool->sparse_set) {
         pool->sparse_set = dlb_realloc(pool->sparse_set, pool->capacity * sizeof(*pool->sparse_set));
+        pool->dense_set = dlb_realloc(pool->dense_set, pool->capacity * sizeof(*pool->dense_set));
+        pool->dense_data = dlb_realloc(pool->dense_data, pool->capacity * pool->elem_size);
     } else {
-        pool->dense_set = dlb_calloc(pool->capacity, pool->elem_size);
         pool->sparse_set = dlb_calloc(pool->capacity, sizeof(*pool->sparse_set));
+        pool->dense_set = dlb_calloc(pool->capacity, sizeof(*pool->dense_set));
+        pool->dense_data = dlb_calloc(pool->capacity, pool->elem_size);
     }
 
     // Initialize intrusive freelist
